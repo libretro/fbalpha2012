b014adeeb1d607ba786ba6162eb4149b94149b04
TwinAphex51224 <twinaphex1@gmail.com>
2012-03-12 04:46:17 +0100

    (Libsnes 360) CPS2 should display properly now - possibly others as well

--- src-0.2.97.23/src/burner/libsnes/big_endian.h
+++ src-0.2.97.23/src/burner/libsnes/big_endian.h
@@ -9,7 +9,7 @@ typedef union {
 
 #if defined(_XBOX)
 #define BURN_ENDIAN_SWAP_INT8(x)				(x^1)
-#define BURN_ENDIAN_SWAP_INT16(x)				((((x) & 0xff) <<  8) | (((x) & 0xff00) >> 8))
+#define BURN_ENDIAN_SWAP_INT16(x)				(_byteswap_ushort(x))
 #define BURN_ENDIAN_SWAP_INT32(x)				(_byteswap_ulong(x))
 #define BURN_ENDIAN_SWAP_INT64(x)				(_byteswap_uint64(x))
 #endif

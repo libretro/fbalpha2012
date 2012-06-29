57ffd18c35ae71c4a85b9f78c4a9d2ab5d61178a
Twinaphex <autechre1024@hotmail.com>
2012-06-30 01:04:35 +0200

    Add FBACores (CPS1/CPS2)

---
+++ src-0.2.97.26/fbacores/cps1_2/generate_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -f src/dep/generated/driverlist.h ]; then
+   rm -rf src/dep/generated
+fi
+
+if [ -d src/dep/generated ]; then
+   echo 'Directory 'src/dep/generated' already exists, skipping creation...'
+else
+   mkdir src/dep/generated
+fi
+
+#generate gamelist.txt and src/dep/generated/driverlist.h
+perl src/dep/scripts/gamelist.pl -o src/dep/generated/driverlist.h -l gamelist.txt src/burn/drv/capcom
+
+#compile m68kmakeecho 
+gcc -o m68kmake src/cpu/m68k/m68kmake.c
+
+#create m68kops.h with m68kmake
+./m68kmake src/cpu/m68k/ src/cpu/m68k/m68k_in.c
+
+g++ -o  ctvmake src/burn/drv/capcom/ctv_make.cpp
+./ctvmake > src/dep/generated/ctv.h

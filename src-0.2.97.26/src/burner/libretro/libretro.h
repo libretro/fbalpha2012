4f67c7ad07bc44963e32201cc00e0f9201ad95aa
twinaphex <autechre1024@hotmail.com>
2012-07-26 18:27:53 +0200

    (Xbox 1) Some tweaks for Xbox 1 - to get around stdint.h missing

--- src-0.2.97.26/src/burner/libretro/libretro.h
+++ src-0.2.97.26/src/burner/libretro/libretro.h
@@ -1,7 +1,15 @@
 #ifndef LIBRETRO_H__
 #define LIBRETRO_H__
 
+#ifdef _XBOX1
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed long int32_t;
+typedef unsigned long uint32_t;
+#else
 #include <stdint.h>
+#endif
 #include <stddef.h>
 
 #ifdef __cplusplus

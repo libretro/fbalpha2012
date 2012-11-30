aa2000b06756b58d879f907f98fb63cd8f6bd46c
twinaphex <autechre1024@hotmail.com>
2012-11-30 04:10:49 +0100

    Android .mk file reupload (with cyclone patch)

--- src-0.2.97.27/projectfiles/jni/Android.mk
+++ src-0.2.97.27/projectfiles/jni/Android.mk
@@ -2,23 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CXXFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-LOCAL_C_FLAGS += -fuse-ld=gold
-LOCAL_CPP_FLAGS+= -fuse-ld=gold
-LOCAL_LDLIBS := -fuse-ld=gold
-endif
 
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CXXFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CXXFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-MAIN_FBA_DIR := ../../src
+MAIN_FBA_DIR := ../../../src
 FBA_BURN_DIR := $(MAIN_FBA_DIR)/burn
 FBA_BURN_DRIVERS_DIR := $(MAIN_FBA_DIR)/burn/drv
 FBA_BURNER_DIR := $(MAIN_FBA_DIR)/burner
@@ -29,6 +14,24 @@ FBA_INTERFACE_DIR := $(MAIN_FBA_DIR)/intf
 FBA_GENERATED_DIR = $(MAIN_FBA_DIR)/dep/generated
 FBA_SCRIPTS_DIR = $(MAIN_FBA_DIR)/dep/scripts
 
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CXXFLAGS += -DANDROID_ARM
+LOCAL_ARM_MODE := arm
+LOCAL_C_FLAGS += -fuse-ld=gold
+LOCAL_CPP_FLAGS+= -fuse-ld=gold
+LOCAL_LDLIBS := -fuse-ld=gold
+CYCLONE_SRC := $(FBA_CPU_DIR)/cyclone/cyclone.s
+CYCLONE_DEFINES := -DBUILD_C68K
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CXXFLAGS +=  -DANDROID_X86
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CXXFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+endif
+
 BURN_BLACKLIST := $(FBA_BURNER_DIR)/un7z.cpp \
 	$(FBA_CPU_DIR)/arm7/arm7exec.c \
 	$(FBA_CPU_DIR)/arm7/arm7core.c \
@@ -36,7 +39,7 @@ BURN_BLACKLIST := $(FBA_BURNER_DIR)/un7z.cpp \
 	$(FBA_CPU_DIR)/hd6309/6309ops.c \
 	$(FBA_CPU_DIR)/konami/konamtbl.c \
 	$(FBA_CPU_DIR)/konami/konamops.c \
-        $(FBA_CPU_DIR)/m68k/m68k_in.c \
+	$(FBA_CPU_DIR)/m68k/m68k_in.c \
 	$(FBA_CPU_DIR)/m6800/6800ops.c \
 	$(FBA_CPU_DIR)/m6800/6800tbl.c \
 	$(FBA_CPU_DIR)/m6805/6805ops.c \
@@ -111,11 +114,10 @@ FBA_SRC_DIRS := $(FBA_BURNER_DIR) $(FBA_BURN_DIRS) $(FBA_CPU_DIRS) $(FBA_BURNER_
 
 LOCAL_MODULE    := libretro
 
-LOCAL_SRC_FILES := $(filter-out $(BURN_BLACKLIST),$(foreach dir,$(FBA_SRC_DIRS),$(wildcard $(dir)/*.cpp))) $(filter-out $(BURN_BLACKLIST),$(foreach dir,$(FBA_SRC_DIRS),$(wildcard $(dir)/*.c))) $(LIBRETRO_DIR)/libretro.cpp $(LIBRETRO_DIR)/neocdlist.cpp
+LOCAL_SRC_FILES := $(CYCLONE_SRC)  $(filter-out $(BURN_BLACKLIST),$(foreach dir,$(FBA_SRC_DIRS),$(wildcard $(dir)/*.cpp))) $(filter-out $(BURN_BLACKLIST),$(foreach dir,$(FBA_SRC_DIRS),$(wildcard $(dir)/*.c))) $(LIBRETRO_DIR)/libretro.cpp $(LIBRETRO_DIR)/neocdlist.cpp 
 
-
-LOCAL_CXXFLAGS += -O3 -fno-stack-protector -DUSE_SPEEDHACKS -DINLINE="static inline" -DSH2_INLINE="static inline" -D__LIBRETRO_OPTIMIZATIONS__ -DLSB_FIRST -D__LIBRETRO__ -Wno-write-strings -DUSE_FILE32API -DANDROID -DFRONTEND_SUPPORTS_RGB565
-LOCAL_CFLAGS = -O3 -fno-stack-protector -DUSE_SPEEDHACKS -DINLINE="static inline" -DSH2_INLINE="static inline" -D__LIBRETRO_OPTIMIZATIONS__ -DLSB_FIRST -D__LIBRETRO__ -Wno-write-strings -DUSE_FILE32API -DANDROID -DFRONTEND_SUPPORTS_RGB565
+LOCAL_CXXFLAGS += -O3 -fno-stack-protector -DUSE_SPEEDHACKS -DINLINE="static inline" -DSH2_INLINE="static inline" -D__LIBRETRO_OPTIMIZATIONS__ -DLSB_FIRST -D__LIBRETRO__ -Wno-write-strings -DUSE_FILE32API -DANDROID -DFRONTEND_SUPPORTS_RGB565 $(CYCLONE_DEFINES)
+LOCAL_CFLAGS = -O3 -fno-stack-protector -DUSE_SPEEDHACKS -DINLINE="static inline" -DSH2_INLINE="static inline" -D__LIBRETRO_OPTIMIZATIONS__ -DLSB_FIRST -D__LIBRETRO__ -Wno-write-strings -DUSE_FILE32API -DANDROID -DFRONTEND_SUPPORTS_RGB565 $(CYCLONE_DEFINES)
 
 LOCAL_C_INCLUDES = $(FBA_BURNER_DIR)/win32 \
 	$(LIBRETRO_DIR) \
@@ -129,21 +131,7 @@ LOCAL_C_INCLUDES = $(FBA_BURNER_DIR)/win32 \
 	$(FBA_INTERFACE_DIR)/cd \
 	$(FBA_BURNER_DIR) \
 	$(FBA_CPU_DIR) \
-	$(FBA_CPU_DIR)/m6809 \
-	$(FBA_CPU_DIR)/m6805 \
-	$(FBA_CPU_DIR)/m6800 \
-	$(FBA_CPU_DIR)/h6280 \
-	$(FBA_CPU_DIR)/nec \
-	$(FBA_CPU_DIR)/hd6309 \
-	$(FBA_CPU_DIR)/arm7 \
 	$(FBA_CPU_DIR)/i8039 \
-	$(FBA_CPU_DIR)/konami \
-	$(FBA_CPU_DIR)/m68k \
-	$(FBA_CPU_DIR)/m6502 \
-	$(FBA_CPU_DIR)/z80 \
-	$(FBA_CPU_DIR)/sh2 \
-	$(FBA_CPU_DIR)/s2650 \
-	$(FBA_CPU_DIR)/arm \
 	$(FBA_LIB_DIR)/zlib \
 	$(FBA_BURN_DIR)/drv/capcom \
 	$(FBA_BURN_DIR)/drv/dataeast \

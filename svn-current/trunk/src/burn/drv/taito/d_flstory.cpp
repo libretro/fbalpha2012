10799508253465b66042591e86c8a4beab354b09
twinaphex <autechre1024@hotmail.com>
2013-02-15 04:09:14 +0100

    Fix Fairyland Story palette

--- svn-current/trunk/src/burn/drv/taito/d_flstory.cpp
+++ svn-current/trunk/src/burn/drv/taito/d_flstory.cpp
@@ -1091,7 +1091,10 @@ static inline void DrvRecalcPalette()
 		b = (d >> 8) & 0x0f;
 		g = (d >> 4) & 0x0f;
 		r = d & 0x0f;
-		DrvPalette[i] = BurnHighCol((r << 4) | r, (g << 4) | g, (b << 4) | b, 0);
+      r = (r << 4) | r;
+      g = (g << 4) | g;
+      b = (b << 4) | b;
+		DrvPalette[i] = BurnHighCol(r, g, b, 0);
 	}
 }
 

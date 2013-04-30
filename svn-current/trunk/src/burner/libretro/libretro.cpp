4b9de70af838c2c477449392516eff70ae363231
twinaphex <autechre1024@hotmail.com>
2013-05-01 01:04:31 +0200

    Patch up extensions

--- svn-current/trunk/src/burner/libretro/libretro.cpp
+++ svn-current/trunk/src/burner/libretro/libretro.cpp
@@ -72,7 +72,7 @@ void retro_get_system_info(struct retro_system_info *info)
    info->library_version = "v0.2.97.28";
    info->need_fullpath = true;
    info->block_extract = true;
-   info->valid_extensions = "iso";
+   info->valid_extensions = "zip|iso";
 }
 
 static void poll_input();
